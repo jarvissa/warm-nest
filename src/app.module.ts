404fe911e3128b6e5c17aeff235db7bac241c6d7
jarvissa <ealiyev12125@ada.edu.az>
2021-06-20 02:42:25 +0400

    Add users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [CatsController],
+  imports: [UsersModule],
 })
 export class AppModule {}
